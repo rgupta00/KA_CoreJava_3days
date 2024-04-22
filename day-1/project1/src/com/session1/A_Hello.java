a6de492da465acad5c96cf3aa700f64c708d9e07
rgupta00 <rgupta.mtech@gmail.com>
2024-04-22 14:59:44 +0530

    date problem

---
+++ day-1/project1/src/com/session1/A_Hello.java
@@ -0,0 +1,48 @@
+package com.session1;
+
+import java.util.Scanner;
+
+public class A_Hello {
+
+	public static void main(String args[]) {
+		System.out.println("printing 5 star");
+		// looping
+		// Nested looping:
+		// technally there is not limit
+		// GPP: nested loop bad in peformance
+//		for (int i = 0; i < 5; i++) {
+//			for (int j = 0; j < 5; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+
+		// GPP: nested loop bad in peformance
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Pls enter no of stars row");
+		
+		int no=scanner.nextInt();
+		
+		for (int i = 0; i < no; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
